b06f523d28779a7a7f5d240b79ec21012848f30b
Robin Avery <robin@ribbanya.dev>
2021-08-24 18:52:22 -0400

    Remove b0xx profile for now (use keymap instead)

--- keyboards/ergodox_ez/keymaps/ribbanya/config.h
+++ keyboards/ergodox_ez/keymaps/ribbanya/config.h
@@ -7,12 +7,6 @@
  *  These options are also useful to firmware size reduction.
  */
 
-/* disable debug print */
-#define NO_DEBUG
-
-/* disable print */
-#define NO_PRINT
-
 /* disable action features */
 //#define NO_ACTION_LAYER
 #define NO_ACTION_TAPPING
