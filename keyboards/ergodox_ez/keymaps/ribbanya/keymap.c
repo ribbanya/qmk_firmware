45053a896ccf9200d9023beceea7371b3c471456
Robin Avery <robin@ribbanya.dev>
2021-08-24 17:58:37 -0400

    Add basic structure for Melee behavior in keymap

--- keyboards/ergodox_ez/keymaps/ribbanya/keymap.c
+++ keyboards/ergodox_ez/keymaps/ribbanya/keymap.c
@@ -1,56 +1,83 @@
 #include QMK_KEYBOARD_H
-#include "version.h"
+#include "melee.h"
 
 enum layers {
-    BASE,  // default layer
-    SYMB,  // symbols
-    MDIA,  // media keys
+    B0XX,  // B0XX (Hax layout)
+    HBOX,  // Hit Box (traditional fighting game)
 };
 
 enum custom_keycodes {
-#ifdef ORYX_CONFIGURATOR
-    VRSN = EZ_SAFE_RANGE,
-#else
-    VRSN = SAFE_RANGE,
-#endif
+    // Hardware buttons
+    GC_A          = JS_BUTTON0,
+    GC_B          = JS_BUTTON1,
+    GC_X          = JS_BUTTON2,
+    GC_Y          = JS_BUTTON3,
+    GC_Z          = JS_BUTTON4,
+    GC_L          = JS_BUTTON5,
+    GC_R          = JS_BUTTON6,
+    GC_START      = JS_BUTTON7,
+    GC_DPAD_UP    = JS_BUTTON8,
+    GC_DPAD_DOWN  = JS_BUTTON9,
+    GC_DPAD_LEFT  = JS_BUTTON10,
+    GC_DPAD_RIGHT = JS_BUTTON11,
+
+    // Analog emulation
+    GC_ANALOG_UP = SAFE_RANGE,
+    GC_ANALOG_DOWN,
+    GC_ANALOG_LEFT,
+    GC_ANALOG_RIGHT,
+    GC_CSTICK_UP,
+    GC_CSTICK_DOWN,
+    GC_CSTICK_LEFT,
+    GC_CSTICK_RIGHT,
+    GC_MX,
+    GC_MY,
+    GC_R_LS,
+    GC_R_MS,
+
+    NEW_SAFE_RANGE
 };
+
+
+gc_input_t gc_input;
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Keymap 0: Basic layer
+/* Keymap 0: B0XX layout
+ * AS = Analog stick. DP = D-pad. CS = C-stick.
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -    |
+ * |  B0XX  | HBOX |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Del    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |        |      |      |      |      |      |      |           |      |   R  |   Y  |   LS |   MS |      |  Start |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | BkSp   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|' / Cmd |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * |        |      |      |  AS← |  AS↓ |  AS→ |------|           |------|   B  |   X  |   Z  |  AS↑ |      |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |   L  |      |      |      |      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |AltShf| Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
+ *                                        |  DP↑ |  DP↓ |       |      |  CS↑   |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
- *                                 |      |ace   | End  |       | PgDn |        |      |
+ *                                 |      |      |  DP← |       | CS←  |        |      |
+ *                                 |  MX  |  MY  |------|       |------|   A    |  CS→ |
+ *                                 |      |      |  DP→ |       | CS↓  |        |      |
  *                                 `--------------------'       `----------------------'
  */
-[BASE] = LAYOUT_ergodox_pretty(
+[B0XX] = LAYOUT_ergodox_pretty(
   // left hand
-  KC_EQL,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,              KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,
-  KC_DEL,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    TG(SYMB),             TG(SYMB),     KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS,
-  KC_BSPC,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,                                        KC_H,    KC_J,    KC_K,    KC_L,    LT(MDIA, KC_SCLN), GUI_T(KC_QUOT),
-  KC_LSFT,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),                  MEH_T(KC_NO), KC_N,    KC_M,    KC_COMM, KC_DOT,           CTL_T(KC_SLSH), KC_RSFT,
-  LT(SYMB,KC_GRV), KC_QUOT,     LALT(KC_LSFT), KC_LEFT, KC_RGHT,                                              KC_UP,   KC_DOWN, KC_LBRC, KC_RBRC, TT(SYMB),
-                                                           ALT_T(KC_APP), KC_LGUI,                KC_LALT, CTL_T(KC_ESC),
-                                                                          KC_HOME,                 KC_PGUP,
-                                                         KC_SPC, KC_BSPC, KC_END,                  KC_PGDN, KC_TAB, KC_ENT
+  DF(B0XX), DF(HBOX), KC_NO, KC_NO, KC_NO,   KC_NO,          KC_NO,              KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO,   KC_NO, KC_NO,
+  KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO,   KC_NO,          KC_NO,              KC_NO, GC_R,  GC_Y,  GC_R_LS,      GC_R_MS, KC_NO, GC_START,
+  KC_NO,    KC_NO,    KC_NO, GC_ANALOG_LEFT, GC_ANALOG_DOWN, GC_ANALOG_RIGHT,    GC_B,  GC_X,  GC_Z,  GC_ANALOG_UP, KC_NO,   KC_NO,
+  KC_NO,    KC_NO,    GC_L,  KC_NO, KC_NO,   KC_NO,          KC_NO,              KC_NO, KC_NO, KC_NO, KC_NO,        KC_NO,   KC_NO, KC_NO,
+  KC_NO,    KC_NO,    KC_NO, KC_NO, KC_NO,                                                     KC_NO, KC_NO,        KC_NO,   KC_NO, KC_NO,
+                                                    GC_DPAD_UP, GC_DPAD_DOWN,    KC_NO, GC_CSTICK_UP,
+                                                                GC_DPAD_LEFT,    GC_CSTICK_LEFT,
+                                                 GC_MX, GC_MY, GC_DPAD_RIGHT,    GC_CSTICK_DOWN, GC_A, GC_CSTICK_RIGHT
 ),
-/* Keymap 1: Symbol Layer
- *
+/* Keymap 1: Hit Box layout
+ * TODO
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * |Version  |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
@@ -70,58 +97,70 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
-[SYMB] = LAYOUT_ergodox_pretty(
-  // left hand
-  VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,     KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS,     KC_TRNS, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
-  KC_TRNS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,               KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_TRNS,
-  KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,     KC_TRNS, KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, KC_TRNS,
-  EEP_RST, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                         KC_TRNS, KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
-                                               RGB_MOD, KC_TRNS,     RGB_TOG, RGB_M_P,
-                                                        KC_TRNS,     KC_TRNS,
-                                      RGB_VAD, RGB_VAI, KC_TRNS,     KC_TRNS, RGB_HUD, RGB_HUI
-),
-/* Keymap 2: Media and mouse keys
- *
- * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
- * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      | MsUp |      |      |      |           |      |      |      |      |      |      |        |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |MsLeft|MsDown|MsRght|      |------|           |------|      |      |      |      |      |  Play  |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      | Prev | Next |      |        |
- * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      | Lclk | Rclk |                                       |VolUp |VolDn | Mute |      |      |
- *   `----------------------------------'                                       `----------------------------------'
- *                                        ,-------------.       ,-------------.
- *                                        |      |      |       |      |      |
- *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |Brwser|
- *                                 |      |      |------|       |------|      |Back  |
- *                                 |      |      |      |       |      |      |      |
- *                                 `--------------------'       `--------------------'
- */
-[MDIA] = LAYOUT_ergodox_pretty(
-  // left hand
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MPLY,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_MPRV, KC_MNXT, KC_TRNS, KC_TRNS,
-  KC_TRNS, KC_TRNS, KC_TRNS, KC_BTN1, KC_BTN2,                                         KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
-
-                                               KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS,
-                                                        KC_TRNS,     KC_TRNS,
-                                      KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_WBAK
-),
+// [HBOX] = LAYOUT_ergodox_pretty(
+//   // left hand
+//   VRSN,    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_TRNS,     KC_TRNS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+//   KC_TRNS, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_TRNS,     KC_TRNS, KC_UP,   KC_7,    KC_8,    KC_9,    KC_ASTR, KC_F12,
+//   KC_TRNS, KC_HASH, KC_DLR,  KC_LPRN, KC_RPRN, KC_GRV,               KC_DOWN, KC_4,    KC_5,    KC_6,    KC_PLUS, KC_TRNS,
+//   KC_TRNS, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_TILD, KC_TRNS,     KC_TRNS, KC_AMPR, KC_1,    KC_2,    KC_3,    KC_BSLS, KC_TRNS,
+//   EEP_RST, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                         KC_TRNS, KC_DOT,  KC_0,    KC_EQL,  KC_TRNS,
+//                                                RGB_MOD, KC_TRNS,     RGB_TOG, RGB_M_P,
+//                                                         KC_TRNS,     KC_TRNS,
+//                                       RGB_VAD, RGB_VAI, KC_TRNS,     KC_TRNS, RGB_HUD, RGB_HUI
+// ),
 };
 // clang-format on
+
+// Runs constantly in the background, in a loop.
+void matrix_scan_user(void) { gc_input.raw = 0; }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch (keycode) {
-            case VRSN:
-                SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
+            case GC_B:
+                gc_input.b = true;
+                return true;
+            case GC_L:
+                gc_input.l_digital = true;
+                return true;
+            case GC_R:
+                gc_input.r_digital = true;
+                return true;
+            case GC_ANALOG_UP:
+                gc_input.analog_up = true;
+                return false;
+            case GC_ANALOG_DOWN:
+                gc_input.analog_down = true;
+                return false;
+            case GC_ANALOG_LEFT:
+                gc_input.analog_left = true;
+                return false;
+            case GC_ANALOG_RIGHT:
+                gc_input.analog_right = true;
+                return false;
+            case GC_CSTICK_UP:
+                gc_input.cstick_up = true;
+                return false;
+            case GC_CSTICK_DOWN:
+                gc_input.cstick_down = true;
+                return false;
+            case GC_CSTICK_LEFT:
+                gc_input.cstick_left = true;
+                return false;
+            case GC_CSTICK_RIGHT:
+                gc_input.cstick_right = true;
+                return false;
+            case GC_MX:
+                gc_input.mod_x = true;
+                return false;
+            case GC_MY:
+                gc_input.mod_y = true;
+                return false;
+            case GC_R_LS:
+                gc_input.light_shield = true;
+                return false;
+            case GC_R_MS:
+                gc_input.mid_shield = true;
                 return false;
         }
     }
---
+++ keyboards/ergodox_ez/keymaps/ribbanya/melee.h
@@ -0,0 +1,20 @@
+typedef union {
+    uint32_t raw;
+    struct {
+        bool b : 1;
+        bool mod_x : 1;
+        bool mod_y : 1;
+        bool l_digital : 1;
+        bool r_digital : 1;
+        bool light_shield : 1;
+        bool mid_shield : 1;
+        bool analog_up : 1;
+        bool analog_down : 1;
+        bool analog_left : 1;
+        bool analog_right : 1;
+        bool cstick_up : 1;
+        bool cstick_down : 1;
+        bool cstick_left : 1;
+        bool cstick_right : 1;
+    };
+} gc_input_t;
